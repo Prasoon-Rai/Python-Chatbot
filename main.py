827eae870ccc54109b9d6181e30b4beb22cd85f8
areno-dev <90553873+areno-dev@users.noreply.github.com>
2021-09-12 15:52:39 +0530

    Python Chatbot made in python !
    You can run this chatbot by using the chatter bot dependency

---
+++ main.py
@@ -0,0 +1,16 @@
+import wolframalpha
+
+# Heart of AI -
+client = wolframalpha.Client('G37W7T-5VTVX57G6L')
+
+def main_chat(query):
+    if "sup" in query:
+        print("Me good !")
+
+while True:
+    query = str(input('Question : '))
+    res = client.query(query)
+    output = next(res.result).text
+    print(output)
+
+    main_chat(output)
