11915a40853c882243c1320b952027ce03202323
Areno <90553873+areno-dev@users.noreply.github.com>
2021-10-14 09:05:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import wolframalpha
 # Heart of AI -
 client = wolframalpha.Client('G37W7T-5VTVX57G6L')
 
-def main_chat(query):
-    if "sup" in query:
-        print("Me good !")
-
 while True:
     query = str(input('Question : '))
     res = client.query(query)
